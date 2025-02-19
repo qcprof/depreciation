ec8fa2bc289de703044ee998087ef68bb96fe7a0
aaq <152490665+isramasjid@users.noreply.github.com>
2025-02-18 20:14:04 -0500

    h1 styling added

--- src/app/straight-line/page.jsx
+++ src/app/straight-line/page.jsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <>
-      <p>Straight Line</p>
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Straight Line</h1>
     </>
   );
 }
