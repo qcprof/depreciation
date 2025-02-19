c2d7701c8109c1b44a557b2c9a473eb13b6c6fa7
aaq <152490665+isramasjid@users.noreply.github.com>
2025-02-18 20:27:10 -0500

    sl calc

--- src/app/straight-line/page.jsx
+++ src/app/straight-line/page.jsx
@@ -1,7 +1,12 @@
 export default function Page() {
+  const cost = 10000
+  const salvageValue = 1000
+  const usefulLife = 3
+  const annualDepreciation = (cost - salvageValue) / usefulLife
   return (
     <>
       <h1 className="ml-5 mb-4 text-3xl text-gray-600 ">Straight Line</h1>
+      <p>Annual depreciation is {annualDepreciation}</p>
     </>
   );
 }
