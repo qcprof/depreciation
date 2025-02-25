cd4e4fdc80d7c6fd4267b6554afd67bd8dad4346
aaq <152490665+isramasjid@users.noreply.github.com>
2025-02-24 20:23:12 -0500

    implemented syd for ul=3

--- src/app/syd/page.jsx
+++ src/app/syd/page.jsx
@@ -2,19 +2,20 @@
 import { useState } from "react";
 
 export default function Page() {
-  const [cost, setCost] = useState(0);
-  const [salvageValue, setSalvageValue] = useState(0);
-  const [usefulLife, setUsefulLife] = useState(0);
+  const [cost, setCost] = useState(10000);
+  const [salvageValue, setSalvageValue] = useState(1000);
+  const [usefulLife, setUsefulLife] = useState(3);
   const [annualDepreciation, setAnnualDepreciation] = useState(0);
 
-  const calculateDepreciation = () => {
-     const depreciation = (cost - salvageValue) / usefulLife;
+  const calculateDepreciation = (year) => {
+    const syd = usefulLife * (usefulLife + 1) / 2
+    const depreciation = (cost - salvageValue) * year/syd;
      setAnnualDepreciation(depreciation);
    };
 
    return (
      <>
-       <h1 className="ml-5 mb-4 text-3xl text-gray-600 ">Straight Line</h1>
+       <h1 className="ml-5 mb-4 text-3xl text-gray-600 ">SYD</h1>
        <div className="space-y-4 max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
          <div className="space-y-2">
            <label
@@ -69,10 +70,23 @@ export default function Page() {
 
          <button
            className="w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-
-           onClick={calculateDepreciation}
+           onClick={() => calculateDepreciation(3)}
          >
-           Calculate Depreciation
+           Calculate Depreciation for Year 1
+         </button>
+
+         <button
+           className="w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+           onClick={() => calculateDepreciation(2)}
+         >
+           Calculate Depreciation for Year 2
+         </button>
+
+         <button
+           className="w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+           onClick={() => calculateDepreciation(1)}
+         >
+           Calculate Depreciation for Year 3
          </button>
        </div>
        <p>Annual depreciation is {annualDepreciation}</p>
