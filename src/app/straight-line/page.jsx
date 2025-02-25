f2607a7e990c3febbf7c04e50c9345123725ed9a
aaq <152490665+isramasjid@users.noreply.github.com>
2025-02-24 19:12:22 -0500

    cost only

--- src/app/straight-line/page.jsx
+++ src/app/straight-line/page.jsx
@@ -3,16 +3,9 @@ import { useState } from "react"
 
 export default function Page() {
   const [cost, setCost] = useState(0)
-  const [salvageValue, setSalvageValue] = useState(0)
-  const [usefulLife, setUsefulLife] = useState(0)
   
-  const annualDepreciation = (cost - salvageValue) / usefulLife
-  // const sydDepreciation
-  // const ddbDepreciation
-
   return (
     <>
-      <h1 className="ml-5 mb-4 text-3xl text-gray-600 ">Straight Line</h1>
       <div className="space-y-4 max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
         <div className="space-y-2">
           <label
@@ -30,44 +23,7 @@ export default function Page() {
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
-
-        <div className="space-y-2">
-          <label
-            htmlFor="sv"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Salvage Value
-          </label>
-          <input
-            type="number"
-            name="sv"
-            id="sv"
-            value={salvageValue}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-
-        <div className="space-y-2">
-          <label
-            htmlFor="ul"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Useful Life
-          </label>
-          <input
-            type="number"
-            name="ul"
-            id="ul"
-            value={usefulLife}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-
-        <button className="w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
-          Calculate
-        </button>
       </div>
-      <p>Annual depreciation is {annualDepreciation}</p>
     </>
   );
 }
